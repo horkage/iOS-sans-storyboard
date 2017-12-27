60deccd2bd0a15d8feaae444907023884c0c3c86
Michael Wood <mwood@Michaels-iMac.local>
2017-12-27 15:02:46 -0800

    Added Constants struct.

---
+++ NoStoryboard/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  NoStoryboard
+//
+//  Created by Michael Wood on 12/27/17.
+//  Copyright © 2017 Michael Wood. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct api {
+        static let baseUrl: String = "http://10.1.20.130:9000/getGuys"
+    }
+}
