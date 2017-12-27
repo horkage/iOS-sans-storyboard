8ca86fd75531eecb28b153d004a6a13e6fce85c6
Michael Wood <mwood@Michaels-iMac.local>
2017-12-27 15:09:40 -0800

    Added WebService delegate.

---
+++ NoStoryboard/WebServiceDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  WebServiceDelegate.swift
+//  NoStoryboard
+//
+//  Created by Michael Wood on 12/27/17.
+//  Copyright © 2017 Michael Wood. All rights reserved.
+//
+
+import Foundation
+
+protocol WebServiceDelegate {
+    func onData(data: [Any])
+}
